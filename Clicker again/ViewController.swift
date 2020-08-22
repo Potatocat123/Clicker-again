82ff4f3e610162a96fee322cf521a6fb6eb1e937
nitesphere08 <69704760+nitesphere08@users.noreply.github.com>
2020-08-22 11:46:25 +0800

    Update ViewController.swift

--- Clicker again/ViewController.swift
+++ Clicker again/ViewController.swift
@@ -17,6 +17,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func buttonPressed(_ sender: Any) {
+        let redValue = CGFloat(drand48())
+        let greenValue = CGFloat(drand48())
+        let blueValue = CGFloat(drand48())
+        self.view.backgroundColor = UIColor (red: redValue,
+        green: greenValue, blue: blueValue, alpha: 1.0)
         counter += 1
         counterLabel.text = "\(counter)"
         
