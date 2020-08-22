c6f8d2554e9183379a59aa4318daebc7d495cf5e
nitesphere08 <69704760+nitesphere08@users.noreply.github.com>
2020-08-22 12:08:18 +0800

    Update ViewController.swift
    hERE TIMER AND ANIMATION

--- Clicker again/ViewController.swift
+++ Clicker again/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var congratsLabel: UILabel!
     @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var clickButton: UIButton!
     var counter = 0
+    var timeElapsed = 0.0
+    var timer: Timer
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -24,8 +28,39 @@ class ViewController: UIViewController {
         green: greenValue, blue: blueValue, alpha: 1.0)
         counter += 1
         counterLabel.text = "\(counter)"
+         if counter >= 10 {
+                  congratsLabel.isHidden = false
+                  congratsLabel.text = "10 clicks!"
+              }
+              
+              if counter == 20 {
+                  // disable button
+                  clickButton.isEnabled = false
+                  clickButton.setTitle("You're done", for: .disabled)
+                  congratsLabel.text = "You took \(timeElapsed) seconds to complete"
+    
+              }
+              
+              self.counterLabel.transform = .identity
+              UIView.animate(withDuration: 0.5) {
+                  self.counterLabel.transform = self.counterLabel.transform
+                      .scaledBy(x: 1.05, y: 1.05)
+              }
+        
+              self.counterLabel.transform = .identity
+              UIView.animate(withDuration: 0.1, animations: {
+
+                  self.counterLabel.transform = self.counterLabel.transform
+                      .scaledBy(x: 1.05, y: 1.05)
+              }) { (_) in
+
+                  UIView.animate(withDuration: 0.1) {
+                      self.counterLabel.transform = self.counterLabel.transform
+                          .scaledBy(x: 0.95, y: 0.95)
+                  }
+              }
+              
         
     }
     
 }
-
